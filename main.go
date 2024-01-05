fad9fdfd7ee37262175c6ba8b1c4dfb2cf893d09
Laika <1317280699@qq.com>
2024-01-05 18:43:02 +0800

    swagger init

--- main.go
+++ main.go
@@ -5,6 +5,14 @@ import (
 	"github.com/ekreke/myTodolist/server"
 )
 
+// @title 这里写标题
+// @version 1.0
+// @description 这里写描述信息
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name 这里写联系人名字
+// @contact.email 这里写联系人邮箱
+
 func main() {
 	conf.DBInit()
 	r := server.NewRouter()
--- server/router.go
+++ server/router.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 
 	"github.com/ekreke/myTodolist/api"
+	_ "github.com/ekreke/myTodolist/docs"
 	"github.com/ekreke/myTodolist/pkg/util/sdk"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // Gin router
@@ -16,6 +19,7 @@ func NewRouter() *gin.Engine {
 	r := gin.Default()
 	// TODO: add middleware
 	// r.Use(middleware.Cors())
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	store := cookie.NewStore([]byte(sdk.VERSION))
 	r.Use(sessions.Sessions("myssion", store))
 
